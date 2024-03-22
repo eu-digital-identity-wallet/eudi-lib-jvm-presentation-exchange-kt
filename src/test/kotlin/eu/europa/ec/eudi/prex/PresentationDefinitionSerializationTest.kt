b3fa7861a8569474e9ddea8a622306805bc5b508
Gennadi Kudrjavtsev <gennadi.kudrjavtsev@cyber.ee>
2024-03-22 15:33:29 +0200

    made Format serializable and added serialization tests that currently fail because of Filter type

---
+++ src/test/kotlin/eu/europa/ec/eudi/prex/PresentationDefinitionSerializationTest.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.europa.ec.eudi.prex
+
+import eu.europa.ec.eudi.prex.TestUtils.deserializeObject
+import eu.europa.ec.eudi.prex.TestUtils.loadResource
+import eu.europa.ec.eudi.prex.TestUtils.serializeObject
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class PresentationDefinitionSerializationTest {
+
+    @Test
+    fun `should serialize format test`() {
+        testPresentationSerialization("v2.0.0/presentation-definition/format_example.json")
+    }
+
+    @Test
+    fun `should serialize basic example`() {
+        testPresentationSerialization("v2.0.0/presentation-definition/basic_example.json")
+    }
+
+    @Test
+    fun `should serialize single group example`() {
+        testPresentationSerialization("v2.0.0/presentation-definition/single_group_example.json")
+    }
+
+    @Test
+    fun `should serialize multi group example`() {
+        testPresentationSerialization("v2.0.0/presentation-definition/multi_group_example.json")
+    }
+
+    @Test
+    fun `should serialize mDL example`() {
+        testPresentationSerialization("v2.0.0/presentation-definition/mDL-example.json")
+    }
+
+    @Test
+    fun `should serialize fi example`() {
+        testPresentationSerialization("v2.0.0/presentation-definition/fi.json")
+    }
+
+    private fun testPresentationSerialization(path: String) {
+        loadResource(path).use {
+            val definition = PresentationExchange.jsonParser.decodePresentationDefinition(it).getOrThrow()
+            val serialized = serializeObject(definition)
+            val deserialized = deserializeObject<PresentationDefinition>(serialized)
+            assertEquals(definition, deserialized)
+        }
+    }
+}
---
+++ src/test/kotlin/eu/europa/ec/eudi/prex/PresentationSubmissionSerializationTest.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.europa.ec.eudi.prex
+
+import eu.europa.ec.eudi.prex.TestUtils.deserializeObject
+import eu.europa.ec.eudi.prex.TestUtils.loadResource
+import eu.europa.ec.eudi.prex.TestUtils.serializeObject
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class PresentationSubmissionSerializationTest {
+
+    private val parser = PresentationExchange.jsonParser
+
+    @Test
+    fun `should serialize simple example`() {
+        testSubmissionSerialization("v2.0.0/presentation-submission/example.json")
+    }
+
+    @Test
+    fun `should serialize OIDC example`() {
+        testSubmissionSerialization("v2.0.0/presentation-submission/appendix_OIDC_example.json")
+    }
+
+    @Test
+    fun `should serialize VP example`() {
+        testSubmissionSerialization("v2.0.0/presentation-submission/appendix_VP_example.json")
+    }
+
+    @Test
+    fun `should serialize JWT example`() {
+        testSubmissionSerialization("v2.0.0/presentation-submission/appendix_JWT_example.json")
+    }
+
+    private fun testSubmissionSerialization(path: String) {
+        loadResource(path).use {
+            val definition = parser.decodePresentationSubmission(it).getOrThrow()
+            val serialized = serializeObject(definition)
+            val deserialized = deserializeObject<PresentationSubmission>(serialized)
+            assertEquals(definition, deserialized)
+        }
+    }
+}
